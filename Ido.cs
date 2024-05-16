5bd527a24ccc71539efe50cfb82a98620b3b7c66
varashus <115972590+varashus@users.noreply.github.com>
2024-05-16 14:05:33 +0200

    Add files via upload

---
+++ Ido.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Weather
+{
+    internal class Ido
+    {
+        public Ido(string r)
+        {
+            var v = r.Split(','); 
+            this.varos =v[0];
+            this.homerseklet = double.Parse(v[1].Replace('.',','));
+            this.para = int.Parse(v[2]);
+            this.szel = double .Parse(v[3].Replace('.', ','));
+
+        }
+
+
+        public string varos { get; set; }
+        public double homerseklet { get; set; }
+        public int para { get; set; }
+        public double szel { get; set; }
+
+        public override string ToString()
+        {
+            
+            return $"{varos}  |  {homerseklet} C˙  |  {para}%  |  {szel}km/h\t";
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Weather
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var vrok = new List<Ido>();
+            using StreamReader sr = new StreamReader(
+                path: @"..\..\..\src\cities.txt",
+                encoding: System.Text.Encoding.UTF8
+                );
+            _ = sr.ReadLine();
+
+            while (!sr.EndOfStream)
+            {
+                vrok.Add(new Ido(sr.ReadLine()));
+            }
+
+
+
+            foreach (var item in vrok)
+            {
+                asd.Items.Add(item);
+            }
+
+         
+
+
+
+
+
+
+        }
+       
+
+        private void Delete_Click(object sender, RoutedEventArgs e)
+        {
+            asd.Items.Remove(asd.SelectedItem);
+
+            if (asd.Items.Count <= 0)
+            {
+                MessageBox.Show("nincs elem amit lehetne törölni");
+            }
+        }
+
+
+
+    }
+}
